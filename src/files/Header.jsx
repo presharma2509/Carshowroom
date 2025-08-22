a410046bde73752073207a482caee3173bddb3b7
user.name : "Prerna Sharma <user.email: "prernasharma0925@gmail.com>
2025-08-22 10:58:43 +0530

    Updated images issue fix

--- src/files/Header.jsx
+++ src/files/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import bgcar from '../assets/black car.avif'
 
 const Header = () => {
   return (
     <div>
-        <img className=' w-screen h-screen fixed -z-10 '  src="src/assets/black car.avif" alt="" />
+        <img className=' w-screen h-screen fixed -z-10 '  src={bgcar} alt="bgcar" />
         {/* <video className='w-screen h-screen fixed -z-10 autofill:*: ' src="src/assets/carvideo.mp4"></video> */}
     </div>
   )
