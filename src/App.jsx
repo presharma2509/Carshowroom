0676b87b290b9926732fd505fb6f48dfb526c5f8
user.name : "Prerna Sharma <user.email: "prernasharma0925@gmail.com>
2025-08-22 22:08:54 +0530

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import {useState} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Products from './components/Products'
 import Contact from './components/Contact'
 import Services from './components/Services'
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
@@ -18,7 +19,7 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
       </Routes>
-    </>
+    </BrowserRouter>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
     <App/>
-  </BrowserRouter>
+  </StrictMode>
+ 
 )
